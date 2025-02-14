f3a454f879c3ce97fbfb87106cc159c7c46309e2
Projeto Integrador SENAC <35633253+wadley-kun@users.noreply.github.com>
2025-02-13 21:04:06 -0300

    Add files via upload

--- JS/section.js
+++ JS/section.js
@@ -2,6 +2,7 @@ document.addEventListener("DOMContentLoaded", () => {
     const cardapioContainer = document.getElementById("Cardapio_doces");
     const carrinhoContainer = document.getElementById("carrinho");
     const listaCarrinho = document.getElementById("listaCarrinho");
+    const descricaoItem = document.getElementById("descricaoItem");
     const contadorCarrinho = document.getElementById("contadorCarrinho");
     const abrirCarrinho = document.getElementById("abrirCarrinho");
     const fecharCarrinho = document.getElementById("fecharCarrinho");
@@ -10,29 +11,14 @@ document.addEventListener("DOMContentLoaded", () => {
     let carrinho = JSON.parse(localStorage.getItem('carrinho')) || [];
 
     const cardapio = [
-        { nome: "Pizza de Catupiry (G)", preco: 34.99, imagem: "../recursos/img/Section_Carrossel/Recomendacoes/Pizza.png", descricao: "Deliciosa pizza de frango com catupiry, feita com ingredientes frescos e selecionados." },
+        { nome: "Pizza de Catupiry (G)", preco: 34.99, imagem: "PI-main/recursos/img/Section_Carrossel/Recomendacoes/Pizza.png", descricao: "Deliciosa pizza de frango com catupiry, feita com ingredientes frescos e selecionados." },
         { nome: "Cachorro-Quente", preco: 8.99, imagem: "../recursos/img/Section_Carrossel/Recomendacoes/Cachorro-quente.png", descricao: "Cachorro-quente com salsicha, molho de tomate, milho, ervilha, batata palha e maionese." },
         { nome: "Hamburguer Gourmet", preco: 29.99, imagem: "../recursos/img/Section_Carrossel/Recomendacoes/Hamburguer.png", descricao: "Delicioso hamburguer gourmet com carne de primeira, queijo cheddar, alface..." },
-        { nome: "Porção de Churrasco", preco: 27.99, imagem: "../recursos/img/Section_Carrossel/Recomendacoes/Porcao_churrasco.png", descricao: "Delicioso hamburguer gourmet com carne de primeira, queijo cheddar, alface..." },
+        { nome: "Porção de Churrasco", preco: 27.99, imagem: "../recursos/img/Section_Carrossel/Recomendacoes/Porcao_churrasco.png", descricao: "Deliciosa porção de churrasco com carne de primeira, linguiça, frango e pão de alho." },
         { nome: "Prato Feito de Churrasco", preco: 19.99, imagem: "../recursos/img/Section_Carrossel/Recomendacoes/Prato_feito.png", descricao: "Delicioso prato feito de churrasco com carne de primeira, arroz, feijão, farofa e salada." }
     ];
 
-    function carregarCardapio() {
-        cardapio.forEach((item, index) => {
-            const card = document.createElement("div");
-            card.classList.add("item");
-
-            card.innerHTML = `
-                <img src="${item.imagem}" alt="${item.nome}">
-                <h3>${item.nome}</h3>
-                <p>${item.descricao}</p>
-                <span class="preco">R$ ${item.preco.toFixed(2)}</span>
-                <button onclick="adicionarAoCarrinho(${index})">Comprar</button>
-            `;
-
-            cardapioContainer.appendChild(card);
-        });
-    }
+    
 
     window.adicionarAoCarrinho = function (index) {
         const item = cardapio[index];
